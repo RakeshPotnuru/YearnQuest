fd979a5125cd367cbb932e3841d2bdad9f05d919
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2021-10-24 09:21:16 +0530

    update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -19,7 +19,7 @@ var config = {
   // can be one of 'email' or 'phone_number_sms'
   identifierType: "email",
   // Add the API key copied from 2nd step
-  apiKey: SAWO_API_KEY,
+  apiKey: "84b47d5c-830f-4679-8147-51dc5cc41ebf",
   // Add a callback here to handle the payload sent by sdk
   onSuccess: (payload) => {
     // Storing the payload in sessionStorage
