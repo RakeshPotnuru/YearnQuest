4d88b749de44543aecd5f45b867ca08242d7ef5e
rwiteshbera <rwiteshbera@gmail.com>
2021-10-24 09:35:54 +0530

    Added API

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 const payload = sessionStorage.getItem("payload");
-// const logOut = document.getElementById('logout');
+const logOut = document.getElementById('logout');
 
 const handleDonation = async (event) => {
   event.preventDefault();
@@ -21,6 +21,7 @@ const handleDonation = async (event) => {
 const handleLogin = () => {
   if (payload) {
     window.location.assign("/");
+   
   }
   window.location.assign("/login");
 };
@@ -30,11 +31,13 @@ const handleLogout = () => {
   window.location.assign("/");
 };
 
-// let keyElement = JSON.parse(sessionStorage.getItem('payload'));
-// const userName = keyElement.customFieldInputValues.Name;
 
-// if(userName!= "") {
-//   document.getElementById("login").style.display = "none";
-//   document.getElementById("logout").style.display = "block";
-//   document.getElementById("username").innerHTML = `<h4>${userName}</h4>`;
-// }
+
+let keyElement = JSON.parse(sessionStorage.getItem('payload'));
+const userName = keyElement.customFieldInputValues.Name;
+
+if(userName!= "") {
+  document.getElementById("login").style.display = "none";
+  document.getElementById("logout").style.display = "block";
+  document.getElementById("username").innerHTML = `<h4>${userName}</h4>`;
+}
